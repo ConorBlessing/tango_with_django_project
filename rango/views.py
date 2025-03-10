4dd2d549a5a4c999a8a702dccf108400024caa50
Conor Blessing <3067724b@student.gla.ac.uk>
2025-03-10 15:13:05 +0000

    thirteenth commit Chapter4 exercises done and all tests passed

--- rango/views.py
+++ rango/views.py
@@ -42,6 +42,7 @@ def show_category(request, category_name_slug):
     print(request.user)
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -55,6 +56,7 @@ def add_category(request):
     print(request.user)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -130,7 +132,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
